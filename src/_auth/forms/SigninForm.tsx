0cece00f9e6362b7c6f5386898334bcbd91743ca
Wagner de Melo Reck <wagner.reck@gmail.com>
2024-08-23 19:06:42 -0300

    build: initial commit

---
+++ src/_auth/forms/SigninForm.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SigninForm = () => {
+  return <div> Signup form</div>;
+};
+
+export default SigninForm;
---
+++ src/_auth/forms/SignupForm.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "../../components/ui/button";
+
+const SignupForm = () => {
+  return (
+    <div>
+      <Button> Sign Up</Button>
+    </div>
+  );
+};
+
+export default SignupForm;
